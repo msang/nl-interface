2d0bbb0bf41db2faf0a2fcf088bae84e2bfdb0d0
msang <manu.sanguin@gmail.com>
2024-03-20 18:59:14 +0100

    cleaned code

--- actions/monitoring.py
+++ actions/monitoring.py
@@ -1,63 +1,40 @@
-import json, os, requests 
-import pandas as pd
-from collections import defaultdict as ddict
-from datetime import datetime
+import os
 from solaredge_interface.api.SolarEdgeAPI import SolarEdgeAPI
 from typing import Text
 from dotenv import find_dotenv, load_dotenv
 
-
+load_dotenv(find_dotenv())
+SE_APIKEY = os.environ.get("SOLAREDGE_KEY")   
 
 class EnergyMonitoring():
     
-     def _init_(self) -> None:
-        pass
-        
-     def get_consumption_data(self) -> Text:   
-        load_dotenv(find_dotenv())
-        SE_APIKEY = os.environ.get("SOLAREDGE_KEY")       
+     def __init__(self) -> None:
         api = SolarEdgeAPI(api_key=SE_APIKEY, datetime_response=True, pandas_response=True)
         SITEID = api.get_sites().pandas['sites.site.id'][0]
         data = api.get_site_current_power_flow(SITEID).data
-        data = data["siteCurrentPowerFlow"]
-        grid = data["GRID"]["currentPower"]
-        pv = data["PV"]["currentPower"]
-        load = data["LOAD"]["currentPower"]
-        storage_stat = data["STORAGE"]["status"]
-        storage_pow =  data["STORAGE"]["currentPower"]
-        storage_lev = data["STORAGE"]["chargeLevel"]
-        grid_import = grid if grid-pv > 0 else 0
-        from_pv = min(load, pv-load) if pv > 0 else 0
+        self.data = data["siteCurrentPowerFlow"]
+        self.grid = self.data["GRID"]["currentPower"]
+        self.pv = self.data["PV"]["currentPower"]
+        self.load = self.data["LOAD"]["currentPower"]
+        self.storage_status = self.data["STORAGE"]["status"]
+        self.storage_pow =  self.data["STORAGE"]["currentPower"]
+        self.storage_lev = self.data["STORAGE"]["chargeLevel"]
+        
+     def get_consumption_data(self) -> Text:       
 
-        #text = f"Al momento stai consumando {load} kW, di cui {grid_import} dalla rete, {from_pv} dal fotovoltaico  \
-        #    e {storage_pow} dalla batteria."
-            #La batteria è carica al {storage_lev}% "
+        from_pv = min(self.load, self.pv-self.load) if self.pv > 0 else 0
 
-        return load, grid_import, from_pv, storage_pow#text
+        return self.load, self.grid, from_pv, self.storage_pow
      
      def get_renewable_data(self) -> Text:          
-        api = SolarEdgeAPI(api_key=SE_APIKEY, datetime_response=True, pandas_response=True)
-        SITEID = api.get_sites().pandas['sites.site.id'][0]
-        data = api.get_site_current_power_flow(SITEID).data
-        data = data["siteCurrentPowerFlow"]
-        grid = data["GRID"]["currentPower"]
-        pv = data["PV"]["currentPower"]
-        load = data["LOAD"]["currentPower"]
-        storage_stat = data["STORAGE"]["status"]
-        storage_pow =  data["STORAGE"]["currentPower"]
-        storage_lev = data["STORAGE"]["chargeLevel"]
-        grid_import = grid if grid-pv > 0 else 0
-        from_pv = min(load, pv-load) if pv > 0 else 0
 
-        #text = f"Al momento stai consumando {load} kW, di cui {grid_import} dalla rete, {from_pv} dal fotovoltaico  \
-        #    e {storage_pow} dalla batteria."
-            #La batteria è carica al {storage_lev}% "
-
-        return load, grid_import, from_pv, storage_pow#text
+        return self.pv, self.storage_lev, self.storage_status
      
 
 if __name__ == "__main__":
     fb = EnergyMonitoring()
+    print(fb.__dict__)
     print(fb.get_consumption_data())
+    print(fb.get_renewable_data())
 
      
