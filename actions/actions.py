18ce8f1c241994b7af140a78ded1dd68e0b326f5
matteoPili <93051059+matteoPili@users.noreply.github.com>
2024-09-17 17:20:41 +0200

    answerOptimizationRequest takes starting time from intent

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from rasa_sdk.events import SlotSet
 import json
 from actions.monitoring import EnergyMonitoring
 from actions.optimization import Appliance, Optimizer, PV
+from datetime import datetime
 #
 #
 
@@ -72,24 +73,32 @@ class AnswerOptimizationRequest(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          
+          device_translation = {
+               "washing_machine": "la lavatrice",
+               "dryer": "l'asciugatrice",
+               "dishwasher": "la lavastoviglie",
+          }
           ##gli slot che deve prendere dall'utente sono relativi all'elettrodomestico e all'orario di avvio
           appliance = tracker.get_slot("device_name")
-          start_time = 7  ### PER ORA DEFINISCO QUI
+          start_time = tracker.get_slot("start_time") 
+          if start_time:
+               # Converte il valore ISO 8601 in oggetto datetime
+               start_time = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
+
           print(appliance)
-          text=""
 
           if appliance is not None:
                # Chiamare il modulo Optimizer con gli slot forniti
-               opt = Optimizer(appliance)
-               pv = PV()
+               #opt = Optimizer(appliance)
+               #pv = PV()
                #pv.set_pv_data(CONFIG_FILE)
-               forecast=pv.get_pv_forecast()
-               _, start_time, model_result = opt.grid_optimizer(start_time, forecast)
-               text = f"Se avvii l'elettrodomestico alle {start_time} consumerai dalla rete {model_result:.2f} kWh. Vuoi saperne di più?"
+               #forecast=pv.get_pv_forecast()
+               #_, start_time, model_result = opt.grid_optimizer(start_time, forecast)
+               model_result = 13.122
+               dispatcher.utter_message(f"Se avvii {device_translation.get(appliance)} alle {start_time.strftime('%H:%M')} consumerai dalla rete {model_result:.2f} kWh. Vuoi saperne di più?")
           else:
                dispatcher.utter_message(text="Mi servirebbe sapere quale elettrodomestico ti interessa")
 
-          dispatcher.utter_message(text=text)
 
           return []
      
